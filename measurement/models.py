5e54373c92b498a7d780e7feea61cbdb54af463c
StarLight-76 <dmitry.kirilchuk@hotmail.com>
2026-07-09 03:10:52 +0300

    Update

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    """Модель датчика"""
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    """Модель измерения температуры"""
+    sensor = models.ForeignKey(
+        Sensor,
+        on_delete=models.CASCADE,
+        related_name='measurements',
+        verbose_name="Датчик"
+    )
+    temperature = models.FloatField(verbose_name="Температура")
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата и время измерения"
+    )
+    # Доп. задание: поле для картинки (опционально)
+    image = models.ImageField(
+        upload_to='measurements/',
+        null=True,
+        blank=True,
+        verbose_name="Фото"
+    )
+
+    def __str__(self):
+        return f"{self.sensor.name} - {self.temperature}°C"
--- measurement/serializers.py
+++ measurement/serializers.py
@@ -1,3 +1,24 @@
 from rest_framework import serializers
+from .models import Sensor, Measurement
 
-# TODO: опишите необходимые сериализаторы
+class MeasurementSerializer(serializers.ModelSerializer):
+    """Сериализатор для измерений (вложенный в датчик)"""
+    class Meta:
+        model = Measurement
+        fields = ['id', 'temperature', 'created_at']
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    """Сериализатор для списка датчиков (краткая информация)"""
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    """Сериализатор для детальной информации по датчику (с измерениями)"""
+    measurements = MeasurementSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
